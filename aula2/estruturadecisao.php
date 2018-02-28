feba81eba5a43d83acf478dd3d1694c8d19c8193
hilton151084 <hiltondavi@yahoo.com.br>
2018-02-27 21:43:22 -0300

    subindo

---
+++ aula2/estruturadecisao.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "<pre>";
+$idade = 15;
+$carteirinha = false;
+
+//if (($idade >= 18) && $carteirinha === true){
+//	echo "Maior Idade e carteirinha";
+//} else if ($carteirinha = false) {
+//	echo "maior de idade faça a carteirinha";
+//} else{
+//	echo "menor Idade";
+//}
+
+$a = "Marcos Lucas";
+
+if ($a == "Lucas") {
+	echo "Ola Lucas";
+} else if ($a == "Marcos"){
+	echo "Olá Marcos";
+}else {
+	echo "Olá estranho";
+}
